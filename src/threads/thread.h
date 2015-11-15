36bf060c612b5a807081a9ad604bf26070553b62
radu-iosif-moldovan <radu.iosif.moldovan@gmail.com>
2015-11-15 18:21:56 +0200

    Added initial priority to struct thread

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -90,6 +90,7 @@ struct thread
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
 
+  	int intitial_priority;				/* Thread's initial priority */
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
